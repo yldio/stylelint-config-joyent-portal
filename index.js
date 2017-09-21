cf0a5107f17a69497019a0d93607fca41ff68424
Sérgio Ramos <mail@sergioramos.me>
2017-09-21 19:02:01 +0100

    disable declaration-empty-line-before

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
   syntax: 'scss',
   processors: ['stylelint-processor-styled-components'],
-  'extends': ['stylelint-config-standard', 'stylelint-config-styled-components']
+  extends: ['stylelint-config-standard', 'stylelint-config-styled-components'],
+  rules: {
+    'declaration-empty-line-before': null,
+    'custom-property-empty-line-before': 'always'
+  }
 };
